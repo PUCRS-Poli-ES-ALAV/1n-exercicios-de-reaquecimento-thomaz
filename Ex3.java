8b87210a340ce8ad66e88a3f86945f13b1473505
Thomaz <141247521+thomazabrantes@users.noreply.github.com>
2025-03-05 00:09:13 +0000

    Ex3 completo

--- Ex3.java
+++ Ex3.java
@@ -1,11 +1,21 @@
+// Cálculo de 1 + 1/2 + 1/3 + 1/4 + ... + 1/N.
+import java.util.Scanner;
 public class Ex3 {
-    public static int somaedivide(int a) {
-        if (a == 0) {
-            return 0;
+    public static double somadiv(int a) {
+        if (a == 1) {
+            return 1.0;
         }
         else {
-            return 1 + somaedivide(1/a);
-            
+            return 1.0/a + somadiv(a - 1);
         }
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Digite um número: ");
+        int a = in.nextInt();
+        double resultado = somadiv(a);
+        System.out.printf("O resultado da soma é: %.6f. \n", resultado);
+        in.close();
     }
 }
